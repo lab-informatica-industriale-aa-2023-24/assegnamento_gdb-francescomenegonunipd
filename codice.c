a608cc3d434b7d68a683143e6009478710371cc9
Francesco Menegon <francesco.menegon.1@studenti.unipd.it>
2024-04-15 19:30:23 +0200

    debug completato

--- codice.c
+++ codice.c
@@ -18,13 +18,13 @@ void estrai_dati(int ac, char **av, int *vett, int *lung)
 
 void fai_spazio(int posizione, int *vett, int lung)
 {
-	for (int j = lung - 1; j > posizione; ++j) 
+	for (int j = lung ; j >= posizione; j--) 
 		vett[j] = vett[j-1];
 }
 
 void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
 { 
-	if (num_dati_ord = 0)  { // il vettore è vuoto, facile
+	if (num_dati_ord == 0)  { // il vettore è vuoto, facile
 		vett[0] = nuovo_dato;
 		return;
 	}
@@ -37,12 +37,14 @@ void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
 			vett[i] = nuovo_dato;
 			return;
 		}
+		else
+			vett[num_dati_ord] = nuovo_dato;
 	}
 }
 
-void ordina_dati(const int *dati_non_ordinati, int *dati_ordinati)
+void ordina_dati(int ac, const int *dati_non_ordinati, int *dati_ordinati)
 {
-	int num_dati = sizeof(dati_non_ordinati) / sizeof(dati_non_ordinati[0]);
+	int num_dati = ac - 1;
 	for (int i = 0; i < num_dati; ++i)
 		inserisci(dati_non_ordinati[i], i, dati_ordinati);
 }
@@ -65,7 +67,7 @@ int main(int argc, char **argv)
 	int num_dati = 0;
 
 	estrai_dati(argc, argv, dati_input, &num_dati);
-	ordina_dati(dati_input, dati_ordinati);
+	ordina_dati(argc, dati_input, dati_ordinati);
 	stampa_vettore(dati_ordinati, num_dati);
 	return 0;
 }
